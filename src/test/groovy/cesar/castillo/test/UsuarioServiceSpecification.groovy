fcc0a87b49818fdbc15a9ebe70fe0eac6a65744d
César Augusto Castillo Farfán <ccastillo@everis.com>
2019-12-30 08:47:40 -0300

    se eleimina los comentarios innecesarios

--- src/test/groovy/cesar/castillo/test/UsuarioServiceSpecification.groovy
+++ src/test/groovy/cesar/castillo/test/UsuarioServiceSpecification.groovy
@@ -21,43 +21,7 @@ class UsuarioServiceSpecification extends Specification {
 	   
 	   usuarioService = null;
 	   usuarioRepository = Stub(UsuarioRepository);
-//	   bookRepository.getBook(_) >> { int id ->
-//		   if (id == 1)
-//		   {
-//				Book b = new Book(1, 'Srujana', 'Spock Tut');
-//				log.debug(b.toString());
-//				return b;
-//		   }
-//			else if (id == 2)
-//		   {
-//			   Book b = new Book(2, 'Eugen', 'JUnit Tut');
-//				log.debug(b.toString());
-//			   return b;
-//		   }
-//		   else if (id == 3)
-//		   {
-//			   log.debug("Book with this ID does not exist");
-//			   return null;
-//		   }
-//	   }
    }
-//   def "retrieved book object is not null"(){
-//	   log.debug ("Feature method 1 - retrieved book object is not null- start");
-//	   expect :
-////		   bookService.retrieveBookDetails(id) != null
-//	   where :
-//	   id << [1, 2]
-//   }
-//	
-//   def "retrieved book object is null"(){
-//	   log.debug ("Feature method - 2 retrieved book object is null - start");
-//	   expect :
-////		   bookService.retrieveBookDetails(id) == null
-//	   where :
-//		
-//	   id << 3
-//		
-//   }
    def cleanup(){
 	   log.info ("Cleanup method - Runs  after every feature method.");
    }
